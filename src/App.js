f6b62f697f97a4abd2ba4ec41ff128960208fabf
Gurwinder <gurwinder95@gmail.com>
2023-06-30 11:48:49 +0530

    consumed restaurant component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
-
+// import Resturant from "./component/Basics/Resturant";cd 
+// import UseState from "./component/Hooks/useState";
+// import UseEffect from "./component/Hooks/useEffect";
+//import UseReducer from "./component/Hooks/useReducer";
 import Restaurant from "./component/basics/Restaurant";
 
 const App = () => {
-  return (
-    <div>Hello World !</div>
-  )
-}
+  return <Restaurant />;
+};
 
 export default App
