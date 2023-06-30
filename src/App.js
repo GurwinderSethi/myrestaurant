788f6b50e34e9058e31f590f186c9fbb1ef69e6a
Gurwinder <gurwinder95@gmail.com>
2023-06-30 12:06:21 +0530

    removed some dump code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React from "react";
-// import Resturant from "./component/Basics/Resturant";cd 
-// import UseState from "./component/Hooks/useState";
-// import UseEffect from "./component/Hooks/useEffect";
-//import UseReducer from "./component/Hooks/useReducer";
+
 import Restaurant from "./component/basics/Restaurant";
 
 const App = () => {
