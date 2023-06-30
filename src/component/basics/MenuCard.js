ac95dd44d24b8a21733bc826ab227b04a720c15f
Gurwinder <gurwinder95@gmail.com>
2023-06-30 14:43:00 +0530

    Destructure

--- src/component/basics/MenuCard.js
+++ src/component/basics/MenuCard.js
@@ -7,21 +7,22 @@ const MenuCard = ({menuData}) => {
     <>
     <section className='main-card--cointainer'>
     {menuData.map((curElement)=>{
+        const {id,name,description,image}=curElement;
         return (
             <>
             
-<div className="card-container" key ={curElement.id}>
+<div className="card-container" key ={id}>
         <div className='card'>
             <div className='card-body'>
-<span className='card-number card-circle subtle'>{curElement.id}</span>
+<span className='card-number card-circle subtle'>{id}</span>
 <span className='card-author subtle'>{curElement.name}</span>
-<h2 className='card-title'>{curElement.name}</h2>
+<h2 className='card-title'>{name}</h2>
 <span className='card-description subtle'>
-    {curElement.description}
+    {description}
 </span>
 <div className="card-read">Read</div>
             </div>
-            <img src={curElement.image} alt = "images" className='class-media'/>
+            <img src={image} alt = "images" className='class-media'/>
             <span className='card-tag subtle'>Order Now</span>
         </div>
     </div>
