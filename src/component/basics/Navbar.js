aa447ba8ff4db1a7d41c0470d779f743f3629eab
Gurwinder <gurwinder95@gmail.com>
2023-06-30 14:43:41 +0530

    added css classes

--- src/component/basics/Navbar.js
+++ src/component/basics/Navbar.js
@@ -3,8 +3,8 @@ import React from "react";
 const navbar = ({ filterItem, menuList }) => {
   return (
     <>
-      <nav className="navbar">
-        <div className="btn-group">
+      <nav className='navbar'>
+        <div className='btn-group'>
           {menuList.map((curElem) => {
             return (
               <button
