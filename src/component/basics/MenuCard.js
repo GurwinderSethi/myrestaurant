efddd2955aa64eab3baf65eadb5903dd17e66a1a
Gurwinder <gurwinder95@gmail.com>
2023-06-30 11:47:34 +0530

    added apis

--- src/component/basics/MenuCard.js
+++ src/component/basics/MenuCard.js
@@ -1,38 +1,37 @@
-import React from "react";
+import React from 'react'
 
-const MenuCard = ({ menuData }) => {
-  //   console.log(menuData);
-
+const MenuCard = ({menuData}) => {
+    console.log(menuData);
+    
   return (
     <>
-      <section className="main-card--cointainer">
-        {menuData.map((curElem) => {
-          const { id, name, category, image, description } = curElem;
-
-          return (
+    <section className='main-card--cointainer'>
+    {menuData.map((curElement)=>{
+        return (
             <>
-              <div className="card-container" key={id}>
-                <div className="card ">
-                  <div className="card-body">
-                    <span className="card-number card-circle subtle">{id}</span>
-                    <span className="card-author subtle"> {category}</span>
-                    <h2 className="card-title"> {name} </h2>
-                    <span className="card-description subtle">
-                      {description}
-                    </span>
-                    <div className="card-read">Read</div>
-                  </div>
-                  <img src={image} alt="images" className="card-media" />
-
-                  <span className="card-tag  subtle">Order Now</span>
-                </div>
-              </div>
-            </>
-          );
-        })}
-      </section>
+            
+<div className="card-container" key ={curElement.id}>
+        <div className='card'>
+            <div className='card-body'>
+<span className='card-number card-circle subtle'>{curElement.id}</span>
+<span className='card-author subtle'>{curElement.name}</span>
+<h2 className='card-title'>{curElement.name}</h2>
+<span className='card-description subtle'>
+    {curElement.description}
+</span>
+<div className="card-read">Read</div>
+            </div>
+            <img src={curElement.image} alt = "images" className='class-media'/>
+            <span className='card-tag subtle'>Order Now</span>
+        </div>
+    </div>
+    
     </>
-  );
-};
+        )
+    })}
+    </section>
+    </>
+  )
+}
 
-export default MenuCard;
+export default MenuCard
