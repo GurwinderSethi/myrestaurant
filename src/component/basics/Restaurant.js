0ab817f2fb9282d02f280621d20285de5fe4bd9a
Gurwinder <gurwinder95@gmail.com>
2023-06-30 14:45:39 +0530

    stored unique category in an array

--- src/component/basics/Restaurant.js
+++ src/component/basics/Restaurant.js
@@ -3,10 +3,35 @@ import "./Style.css";
 import Menu from './menuApi.js';
 import MenuCard from './MenuCard';
 
+const uniqueList = [
+  ... new Set(
+    Menu.map((curElem)=>{
+     return curElem.category;
+        })
+  ),
+];
+
+console.log(uniqueList);
+
 const Restaurant = () => {
     const [menuData,setmenuData]=useState(Menu);
+    const filterItem = (category)=>{
+      const updatedList = Menu.filter((curElem)=>{
+        return curElem.category === category;
+      });
+      setmenuData(updatedList);
+    };
   return (
     <>
+    <nav className='navbar'>
+      <div className='btn-group'>
+        <button className='btn-group__item' onClick={()=>filterItem("breakfast")}>Breakfast</button>
+        <button className='btn-group__item'>Lunch</button>
+        <button className='btn-group__item'>Evening</button>
+        <button className='btn-group__item'>Dinner</button>
+        <button className='btn-group__item' onClick ={()=>{setmenuData(Menu)}}>All</button>
+      </div>
+    </nav>
     <MenuCard menuData={menuData}/>
     </>
   )
