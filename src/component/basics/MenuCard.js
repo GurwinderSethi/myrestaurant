5da331e22d43b88592a85d88511aea3d14ca28b7
Gurwinder <gurwinder95@gmail.com>
2023-07-04 22:07:13 +0530

    text formated

--- src/component/basics/MenuCard.js
+++ src/component/basics/MenuCard.js
@@ -14,13 +14,13 @@ const MenuCard = ({menuData}) => {
 <div className="card-container" key ={id}>
         <div className='card'>
             <div className='card-body'>
-<span className='card-number card-circle subtle'>{id}</span>
-<span className='card-author subtle'>{curElement.name}</span>
-<h2 className='card-title'>{name}</h2>
-<span className='card-description subtle'>
-    {description}
-</span>
-<div className="card-read">Read</div>
+            <span className='card-number card-circle subtle'>{id}</span>
+            <span className='card-author subtle'>{curElement.name}</span>
+            <h2 className='card-title'>{name}</h2>
+            <span className='card-description subtle'>
+            {description}
+            </span>
+            <div className="card-read">Read</div>
             </div>
             <img src={image} alt = "images" className='class-media'/>
             <span className='card-tag subtle'>Order Now</span>
